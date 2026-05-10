407ab5d354ccff7314403c607a7832f721231e0b
Erind Balaj <lowfione2@gmail.com>
2026-05-10 23:57:04 +0200

    Movie Details Page

---
+++ cinema-frontend/src/pages/moviedetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MovieDetails from '../components/MovieDetails'
+
+export default function MovieDetailsPage() {
+  return (
+    <div className="min-h-screen bg-stage">
+      <MovieDetails />
+    </div>
+  )
+}
